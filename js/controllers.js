b041617134fa8550dab872aaef9ccc529c6bd8cc
Danielle "Dani" Moss <dani.j.moss@gmail.com>
2016-04-25 21:46:47 -0400

    Updated site

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@ var characterControllers = angular.module('characterControllers', ['ngAnimate'])
 characterControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
 
 	$http.get('js/data.json').success(function(data){
-		$scope.character = data;
+		$scope.characters = data;
 		$scope.characterOrder = 'name';
 	});	
 
@@ -12,18 +12,18 @@ characterControllers.controller('ListController', ['$scope', '$http', function($
 characterControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
 
 	$http.get('js/data.json').success(function(data){
-		$scope.character = data;
+		$scope.characters = data;
 		$scope.whichItem = $routeParams.itemId;
 
 		if($routeParams.itemId > 0){
 			$scope.prevItem = Number($routeParams.itemId)-1;
 		}
 		else{
-			$scope.prevItem = $scope.character.length-1;
+			$scope.prevItem = $scope.characters.length-1;
 		}	
 
 
-		if($routeParams.itemId < $scope.character.length-1){
+		if($routeParams.itemId < $scope.characters.length-1){
 			$scope.nextItem = Number($routeParams.itemId)+1;
 		}
 		else{
